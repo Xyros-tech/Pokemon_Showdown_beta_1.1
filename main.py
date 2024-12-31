b7521ca37329e7302e266c062aa919511d6b69b6
Xyros-tech <simsekihsan0707@gmail.com>
2024-12-31 19:50:06 +0300

    Pokemon

--- main.py
+++ main.py
@@ -4,7 +4,44 @@ from config import token
 from logic import Pokemon
 import random
 from logic import Wizard
-from logic import Fighter
+from logic import Fighter   # Botun tokenini config.py dosyasından içe aktarma
+
+intents = discord.Intents.default()  # Botun niyetlerini belirlemek için bir intents nesnesi oluşturma
+intents.members = True  # Botun kullanıcılarla çalışmasına ve onları banlamasına izin veren bayrağı ayarlama
+intents.message_content = True  # Botun mesajların içeriğiyle çalışmasına izin veren bayrağın ayarlanması
+
+bot = commands.Bot(command_prefix='!', intents=intents)  # "!" komut önekiyle bir bot örneği oluşturun ve intents nesnesini ona aktarın
+
+@bot.event  # Bot başarıyla başlatıldığında tetiklenecek olayı tanımlama
+async def on_ready():
+    print(f'Giriş yapıldı:  {bot.user.name}')  # Discord'da başarılı oturum açma hakkında konsolda bir mesaj görüntüleyin
+
+@bot.command()  # Kullanıcı "!start" girdiğinde çağrılacak "start" komutunu tanımlayın
+async def start(ctx):
+    await ctx.send("Merhaba! Ben bir sohbet yöneticisi botuyum!")  # Sohbet odasına geri mesaj gönderme
+
+@bot.command()  # Kullanıcının banlanma haklarına sahip olmasını gerektiren "ban" komutunun tanımlanması
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member = None):
+    if member:  # Komutun banlanması gereken kullanıcıyı belirtip belirtmediğinin kontrol edilmesi
+        if ctx.author.top_role <= member.top_role:
+            await ctx.send("Eşit veya daha yüksek rütbeli bir kullanıcıyı yasaklamak mümkün değildir!")
+        else:
+            await ctx.guild.ban(member)  # Bir kullanıcıyı sunucudan banlama
+            await ctx.send(f" Kullanıcı {member.name} banlandı.")  # Başarılı bir banlama hakkında mesaj gönderme
+    else:
+        await ctx.send("Bu komut banlamak istediğiniz kullanıcıyı işaret etmelidir. Örneğin: `!ban @user`")
+
+@ban.error  # "ban" komutu için bir hata işleyicisi tanımlayın
+async def ban_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("Bu komutu çalıştırmak için yeterli izniniz yok.")  # Kullanıcıyı erişim hakları hatası hakkında bilgilendiren bir mesaj gönderme
+    elif isinstance(error, commands.MemberNotFound):
+        await ctx.send("Kullanıcı bulunamadı.")  # Belirtilen kullanıcı bulunamazsa bir hata mesajı gönderme
+
+bot.run(token)  # Kimlik doğrulama için token kullanarak botu başlatma
+
+
 print("Hello Umut Hocam:)")
 print("Hello İhsan")
 # Bot için niyetleri (intents) ayarlama
@@ -64,3 +101,4 @@ async def attack(ctx):
 
 
 bot.run(token)
+
